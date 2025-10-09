563e9994368c80a20dbc26db2ac7f4e5f5a7e53d
Ugur Guenduez <ugur.guenduez@mercedes-benz.com>
2025-10-09 19:54:49 +0200

    Monkey-Bar first commit

--- docs/_next/static/chunks/3435dbed0e8dab63.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/a9f82daf4a610950.js",
-  "static/chunks/turbopack-e9dfc556867f74d5.js"
-])
--- docs/_next/static/chunks/c7843e658c4306fa.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/a9f82daf4a610950.js",
-  "static/chunks/turbopack-25082b5bcd72ec62.js"
-])
